df77c99fae7122bec56a5d65caf8df0409bc1d76
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2017-10-29 14:12:06 -0700

    Fix SudokuGrid3X3 unit tests

--- test/unit/component/sudoku_grid_3x3/sudoku_grid_3x3.test.js
+++ test/unit/component/sudoku_grid_3x3/sudoku_grid_3x3.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable object-property-newline */
 
 import React from "react";
-import {mount, shallow} from "enzyme";
+import {mount} from "enzyme";
 
 import SudokuGrid3X3 from "sudoku_react/component/sudoku_grid_3x3";
 
@@ -12,65 +12,62 @@ const renderComponent = (props) => mount(
 
 
 describe("<SudokuGrid3X3 />", () => {
-    it("should render 9 empty <input> tags", () => {
-        const renderedComponent = renderComponent({
-            fixedCells: [],
-            onChange: () => {},
-        });
-        const inputs = renderedComponent.find("input");
-        expect(inputs.length).toEqual(9);
-        inputs.forEach((input) =>
-            expect(input.prop("value")).toEqual("")
+    it("should render 9 SudokuCell components", () => {
+        const renderedComponent = renderComponent({onChange: () => {}});
+        const cells = renderedComponent.find("SudokuCell");
+        expect(cells.length).toEqual(9);
+        cells.forEach((node) =>
+            expect(node.prop("data")).toEqual([1, 2, 3, 4, 5, 6, 7, 8])
         );
     });
-    it("should render 9 <input> tags filled with a number", () => {
+
+    it("should render 9 SudokuCell filled with data", () => {
         const renderedComponent = renderComponent({
-            c00: 1, c01: 2, c02: 3,
+            c00: 1, c01: 2, c02: [1, 2, 4, 5],
             c10: 4, c11: 5, c12: 6,
-            c20: 7, c21: 8, c22: 9,
-            fixedCells: [],
+            c20: [1, 2], c21: 8, c22: 9,
             onChange: () => {},
         });
 
-        const inputValues = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        const data = [1, 2, [1, 2, 4, 5], 4, 5, 6, [1, 2], 8, 9];
 
-        const inputs = renderedComponent.find("input");
-        expect(inputs.length).toEqual(9);
-        inputs.forEach((input, index) => {
-            expect(input.prop("value")).toEqual(inputValues[index]);
+        const cells = renderedComponent.find("SudokuCell");
+        expect(cells.length).toEqual(9);
+        cells.forEach((node, index) => {
+            expect(node.prop("data")).toEqual(data[index]);
         });
     });
+
     it("should update a cell of the grid", () => {
         const onChangeSpy = jest.fn();
-        const renderedComponent = renderComponent({
-            fixedCells: [],
-            onChange: onChangeSpy,
-        });
+        const renderedComponent = renderComponent({onChange: onChangeSpy});
 
-        const input = shallow(renderedComponent.find("input").get(0));
-
-        expect(input.prop("value")).toEqual("");
-
-        const inputs = renderedComponent.find("input");
-        inputs.forEach((_input) => {
-            _input.simulate("keydown", {key: "8"});
-        });
-
-        expect(onChangeSpy).toHaveBeenCalledTimes(9);
-
-        const grid = {
-            c00: 0, c01: 0, c02: 0,
-            c10: 0, c11: 0, c12: 0,
-            c20: 0, c21: 0, c22: 0,
-        };
-
-        const keys = [
-            "c00", "c01", "c02", "c10", "c11", "c12", "c20", "c21", "c22",
+        const identifiers = [
+            "c00", "c01", "c02",
+            "c10", "c11", "c12",
+            "c20", "c21", "c22",
         ];
 
-        const expected = keys.map(
-            (key) => [Object.assign({}, grid, {[key]: 8})]
-        );
-        expect(onChangeSpy.mock.calls).toEqual(expected);
+        const grid = {
+            c00: [1, 2, 3, 4, 5, 6, 7, 8],
+            c01: [1, 2, 3, 4, 5, 6, 7, 8],
+            c02: [1, 2, 3, 4, 5, 6, 7, 8],
+            c10: [1, 2, 3, 4, 5, 6, 7, 8],
+            c11: [1, 2, 3, 4, 5, 6, 7, 8],
+            c12: [1, 2, 3, 4, 5, 6, 7, 8],
+            c20: [1, 2, 3, 4, 5, 6, 7, 8],
+            c21: [1, 2, 3, 4, 5, 6, 7, 8],
+            c22: [1, 2, 3, 4, 5, 6, 7, 8],
+        };
+
+        const cells = renderedComponent.find("SudokuCell");
+        cells.forEach((node, index) => {
+            expect(node.prop("data")).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+            node.prop("onChange")(5);
+
+            expect(onChangeSpy).toHaveBeenLastCalledWith(
+                Object.assign({}, grid, {[identifiers[index]]: 5})
+            );
+        });
     });
 });
